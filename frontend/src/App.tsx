0e3dc498b87e5751fea2ef758565509f88e5a8a8
Matheus Colin <mattxnice@gmail.com>
2022-11-16 03:14:28 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
   return (
-    <h1>Hello world!</h1>
-  )
+    <>
+      <h1>Hello world!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      
+    </>
+  );
 }
 
-export default App
+export default App;
