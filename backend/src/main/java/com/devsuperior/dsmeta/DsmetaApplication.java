45379c6aefe6625ebbbeff8e2f819e5611e7e7f4
Matheus Colin <mattxnice@gmail.com>
2022-11-18 02:59:23 -0300

    Security Config

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -1,4 +1,4 @@
-package com.devsuperior;
+package com.devsuperior.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/devsuperior/dsmeta/DsmetaApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dsmeta/DsmetaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.devsuperior;
+package com.devsuperior.dsmeta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
